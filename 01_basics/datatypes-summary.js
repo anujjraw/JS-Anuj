a243a1adf7b3972b4d37e536606d5e44114f2291
anujjraw <anujrawat6685@gmail.com>
2025-07-16 19:08:42 +0530

    data types

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -24,7 +24,7 @@ console.log(id === anotherId);
 
 const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
-    name: "hitesh",
+    name: "Anuj",
     age: 22,
 }
 
@@ -35,3 +35,5 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
